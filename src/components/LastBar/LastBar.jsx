4bef3475c5ee13d4fd961bd656789e879cf7ff2e
Elchin901 <73276152+Elchin901@users.noreply.github.com>
2024-07-19 09:52:16 +0200

    ok

--- src/components/LastBar/LastBar.jsx
+++ src/components/LastBar/LastBar.jsx
@@ -5,9 +5,15 @@ export default function LastBar() {
     <nav className="bg-black">
         <div className="flex justify-around">
             <div className="w-1/3">
+            <div>
+
+            </div>
 
             </div>
             <div className="w-1/3">
+            <div>
+              
+            </div>
 
             </div>
 
